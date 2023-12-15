4b7a2c84c7d3edfffce77528826ca8b35ea41c0d
MarcosP-Costa <marcos_nokbuk@hotmail.com>
2023-12-15 05:45:34 -0300

    Arrumei chave API

--- chatgpt.py
+++ chatgpt.py
@@ -3,19 +3,23 @@ import json
 
 
 def gerarTexto(livro):
-    key_chatgpt = "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"
-    headers = {"Authorization": f"Bearer {key_chatgpt}", "Content-Type": "application/json"}
-    link = "https://api.openai.com/v1/chat/completions"
-    modelo = "gpt-3.5-turbo"
-    body = {
-        "model": modelo,
-        "messages": [{"role": "user", "content": f"me resuma {livro} com no máximo 400 caracteres, caso não conheça o livro, responda com Livro Desconhecido"}]
-    }
-    body = json.dumps(body)
-    requisicao = requests.post(link, headers=headers, data=body)
-    resposta = requisicao.json()
-    sinopse = resposta["choices"][0]["message"]["content"]
-    return sinopse
+    try:
+        key_chatgpt = "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"
+        headers = {"Authorization": f"Bearer {key_chatgpt}", "Content-Type": "application/json"}
+        link = "https://api.openai.com/v1/chat/completions"
+        modelo = "gpt-3.5-turbo"
+        body = {
+            "model": modelo,
+            "messages": [{"role": "user", "content": f"me resuma {livro} com no máximo 400 caracteres, caso não conheça o livro, responda com Livro Desconhecido"}]
+        }
+        body = json.dumps(body)
+        requisicao = requests.post(link, headers=headers, data=body)
+        resposta = requisicao.json()
+        sinopse = resposta["choices"][0]["message"]["content"]
+        return sinopse
+    except:
+        return "Chave API Esta fora no momento!"
+    
 
 
 
